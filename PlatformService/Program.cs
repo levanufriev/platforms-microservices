6de6cb8e2f36eefe1606788ab0339dec5f76a755
Lev Anufriev <levanufriev02@gmail.com>
2023-06-17 15:14:44 +0300

    Add platform service.

---
+++ PlatformService/Program.cs
@@ -0,0 +1,32 @@
+using Microsoft.EntityFrameworkCore;
+using PlatformService.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(opt =>
+    opt.UseInMemoryDatabase("InMem"));
+
+builder.Services.AddScoped<IPlatformRepo, PlatformRepo>();
+
+builder.Services.AddControllers();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.PrepPopulation();
+
+app.Run();
